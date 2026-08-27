68c02f2a5e9683a83e86d429baa7db2a7c7a921e
Matvey <matveygorlov.2011@gmail.com>
2026-08-27 18:18:38 +0300

    commit_02

---
+++ task-04.py
@@ -0,0 +1,92 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+
+        except ValueError:
+            return "Give me name and phone please."
+
+        except KeyError:
+            return "Contact not found."
+
+        except IndexError:
+            return "Enter user name"
+
+    return inner
+
+
+def parse_input(user_input):
+    parts = user_input.strip().split()
+    command = parts[0].lower()
+    args = parts[1:]
+    return command, args
+
+
+@input_error
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+
+@input_error
+def change_contact(args, contacts):
+    name, phone = args
+
+    contacts[name] = phone
+    return "Contact updated."
+
+
+@input_error
+def show_phone(args, contacts):
+    name = args[0]
+    return contacts[name]
+
+
+def show_all(contacts):
+    if not contacts:
+        return "No contacts."
+
+    result = ""
+
+    for name, phone in contacts.items():
+        result += f"{name}: {phone}\n"
+
+    return result
+
+
+def main():
+    contacts = {}
+
+    print("Welcome to the assistant bot!")
+
+    while True:
+        user_input = input("Enter a command: ")
+
+        command, args = parse_input(user_input)
+
+        if command == "hello":
+            print("How can I help you?")
+
+        elif command == "add":
+            print(add_contact(args, contacts))
+
+        elif command == "change":
+            print(change_contact(args, contacts))
+
+        elif command == "phone":
+            print(show_phone(args, contacts))
+
+        elif command == "all":
+            print(show_all(contacts))
+
+        elif command == "close" or command == "exit":
+            print("Good bye!")
+            break
+
+        else:
+            print("Invalid command.")
+
+
+if __name__ == "__main__":
+    main()
